4d8379be15ad0b27f5118cc31afcd963d7ee405f
Mishra-coder <devendramishra495@gmail.com>
2026-04-03 22:42:59 +0530

    add /preview route with phone mockup frame

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,18 @@ import { createRoot } from 'react-dom/client'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 import './index.css'
 import App from './App.jsx'
+import Preview from './Preview.jsx'
+
+const isPreview = window.location.pathname === '/preview';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId="1076957044730-mht8sbihb0d4661hprrvbjf9v4gb0njr.apps.googleusercontent.com">
-      <App />
-    </GoogleOAuthProvider>
+    {isPreview ? (
+      <Preview />
+    ) : (
+      <GoogleOAuthProvider clientId="1076957044730-mht8sbihb0d4661hprrvbjf9v4gb0njr.apps.googleusercontent.com">
+        <App />
+      </GoogleOAuthProvider>
+    )}
   </StrictMode>,
 )
